caf724b1f3a315d77cde6b885363ffd72b8ff821
Michael James <hello@michaelbjames.com>
2019-02-19 14:44:07 -0800

    update with zhengs changes

--- src/PetriNet/Abstraction.hs
+++ src/PetriNet/Abstraction.hs
@@ -2,13 +2,15 @@ module PetriNet.Abstraction where
 
 import PetriNet.AbstractType
 import Synquid.Type
+import Synquid.Program
 import PetriNet.PNSolver
 
 import qualified Data.Set as Set
+import Control.Lens
 
-firstLvAbs :: [RSchema] -> AbstractionTree
-firstLvAbs schs =
-    Set.foldl' updateSemantic (ALeaf (AExclusion Set.empty)) dts
+firstLvAbs :: Environment -> [RSchema] -> AbstractionTree
+firstLvAbs env schs =
+    Set.foldl' (updateSemantic env) (ALeaf (AExclusion Set.empty)) dts
   where
     typs = map (shape . toMonotype) schs
-    dts = Set.unions (map allAbstractDts typs)
+    dts = Set.unions (map (allAbstractDts (env ^. boundTypeVars)) typs)
