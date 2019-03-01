a9e22bcb09c7b45d8b16af54b5afadd0856980fe
Michael James <hello@michaelbjames.com>
2019-02-28 18:39:55 -0800

    icfp-t2

---
+++ scripts/icfp-tier2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp libraries/{ghc-prim,base,bytestring,deepseq}.txt $TMPDIR
+
+stack exec -- synquid generate \
+      -p base \
+      -m "Data.Int" \
+    -m "Data.Maybe" \
+    -m "Data.Either" \
+    -m "GHC.List" \
+    -m "GHC.Char" \
+    -m "Text.Show" \
+    -p bytestring \
+    -m "Data.ByteString.Lazy" \
+    -m "Data.ByteString.Builder"
