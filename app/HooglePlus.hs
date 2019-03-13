32df4131445e9608f97ab91d00bda9727845b2c3
Michael James <hello@michaelbjames.com>
2019-03-13 16:11:21 -0700

    reduced params

--- app/HooglePlus.hs
+++ app/HooglePlus.hs
@@ -62,28 +62,10 @@ releaseDate = fromGregorian 2019 3 10
 main = do
   res <- cmdArgsRun $ mode
   case res of
-    (Synthesis file libs onlyGoals envPath
-               appMax scrutineeMax matchMax auxMax genPreds explicitMatch unfoldLocals partial incremental consistency memoize symmetry
-               lfp bfs
-               out_file out_module outFormat resolve
-               print_spec print_stats log_
-               graph succinct sol_num path_search higher_order encoder refine) -> do
+    (Synthesis file libs envPath appMax log_ sol_num path_search higher_order encoder refine) -> do
                   let explorerParams = defaultExplorerParams {
                     _eGuessDepth = appMax,
-                    _scrutineeDepth = scrutineeMax,
-                    _matchDepth = matchMax,
-                    _auxDepth = auxMax,
-                    _predPolyRecursion = genPreds,
-                    _abduceScrutinees = not explicitMatch,
-                    _unfoldLocals = unfoldLocals,
-                    _partialSolution = partial,
-                    _incrementalChecking = incremental,
-                    _consistencyChecking = consistency,
-                    _useMemoization = memoize,
-                    _symmetryReduction = symmetry,
                     _explorerLogLevel = log_,
-                    _buildGraph = graph,
-                    _useSuccinct = succinct,
                     _solutionCnt = sol_num,
                     _pathSearch = path_search,
                     _useHO = higher_order,
@@ -91,11 +73,6 @@ main = do
                     _useRefine = refine
                     }
                   let synquidParams = defaultSynquidParams {
-                    goalFilter = liftM (splitOn ",") onlyGoals,
-                    outputFormat = outFormat,
-                    resolveOnly = resolve,
-                    showSpec = print_spec,
-                    showStats = print_stats,
                     envPath = envPath
                   }
                   runOnFile synquidParams explorerParams file
@@ -117,35 +94,12 @@ data CommandLineArgs
         -- | Input
         file :: String,
         libs :: [String],
-        only :: Maybe String,
         env_file_path_in :: String,
         -- | Explorer params
         app_max :: Int,
-        scrutinee_max :: Int,
-        match_max :: Int,
-        aux_max :: Int,
-        generalize_preds :: Bool,
-        explicit_match :: Bool,
-        unfold_locals :: Bool,
-        partial :: Bool,
-        incremental :: Bool,
-        consistency :: Bool,
-        memoize :: Bool,
-        symmetry :: Bool,
-        -- | Solver params
-        lfp :: Bool,
-        bfs_solver :: Bool,
         -- | Output
-        out_file :: Maybe String,
-        out_module :: Maybe String,
-        output :: OutputFormat,
-        resolve :: Bool,
-        print_spec :: Bool,
-        print_stats :: Bool,
         log_ :: Int,
         -- | Graph params
-        graph :: Bool,
-        succinct :: Bool,
         sol_num :: Int,
         path_search :: PathStrategy,
         higher_order :: Bool,
@@ -165,39 +119,15 @@ data CommandLineArgs
 synt = Synthesis {
   file                = ""              &= typFile &= argPos 0,
   libs                = []              &= args &= typ "FILES",
-  only                = Nothing         &= typ "GOAL,..." &= help ("Only synthesize the specified functions"),
   env_file_path_in    = defaultEnvPath  &= help ("Environment file path (default:" ++ (show defaultEnvPath) ++ ")"),
   app_max             = 6               &= help ("Maximum depth of an application term (default: 6)") &= groupname "Explorer parameters",
-  scrutinee_max       = 1               &= help ("Maximum depth of a match scrutinee (default: 1)"),
-  match_max           = 0               &= help ("Maximum depth of matches (default: 2)"),
-  aux_max             = 1               &= help ("Maximum depth of auxiliary functions (default: 1)") &= name "x",
-  generalize_preds    = True            &= help ("Make recursion polymorphic in abstract refinements (default: False)"),
-  explicit_match      = False           &= help ("Do not abduce match scrutinees (default: False)"),
-  unfold_locals       = False           &= help ("Use all variables, as opposed to top-level function arguments only, in match scrutinee abduction (default: False)"),
-  partial             = False           &= help ("Generate best-effort partial solutions (default: False)") &= name "p",
-  incremental         = True            &= help ("Subtyping checks during bottom-up phase (default: True)"),
-  consistency         = True            &= help ("Check incomplete application types for consistency (default: True)"),
-  memoize             = False           &= help ("Use memoization (default: False)") &= name "z",
-  symmetry            = False           &= help ("Use symmetry reductions (default: False)") &= name "s",
-  lfp                 = False           &= help ("Use least fixpoint solver (only works for type checking, default: False)") &= groupname "Solver parameters",
-  bfs_solver          = False           &= help ("Use BFS instead of MARCO to solve second-order constraints (default: False)"),
-  resolve             = False           &= help ("Resolve only; no type checking or synthesis (default: False)"),
-  out_file            = Nothing         &= help ("Generate Haskell output file (default: none)") &= typFile &= name "o" &= opt "" &= groupname "Output",
-  out_module          = Nothing         &= help ("Name of Haskell module to generate (default: from file name)") &= typ "Name",
-  output              = defaultFormat   &= help ("Output format: Plain, Ansi or Html (default: " ++ show defaultFormat ++ ")") &= typ "FORMAT",
-  print_spec          = True            &= help ("Show specification of each synthesis goal (default: True)"),
-  print_stats         = False           &= help ("Show specification and solution size (default: False)"),
   log_                = 0               &= help ("Logger verboseness level (default: 0)") &= name "l",
-  graph               = False           &= help ("Build graph for exploration (default: False)") &= name "graph",
-  succinct            = False           &= help ("Use graph to direct the term exploration (default: False)") &= name "succ",
   sol_num             = 1               &= help ("Number of solutions need to find (default: 1)") &= name "cnt",
   path_search         = PetriNet     &= help ("Use path search algorithm to ensure the usage of provided parameters (default: PetriNet)") &= name "path",
   higher_order        = False           &= help ("Include higher order functions (default: False)"),
   encoder             = HEncoder.Normal &= help ("Choose normal or refined arity encoder (default: Normal)"),
   use_refine          = PNS.QueryRefinement    &= help ("Use abstract refinement or not (default: QueryRefinement)")
   } &= auto &= help "Synthesize goals specified in the input file"
-    where
-      defaultFormat = outputFormat defaultSynquidParams
 
 generate = Generate {
   pkg_name             = []              &= help ("Package names to be generated"),
@@ -215,24 +145,9 @@ mode = cmdArgsMode $ modes [synt, generate] &=
 -- | Parameters for template exploration
 defaultExplorerParams = ExplorerParams {
   _eGuessDepth = 3,
-  _scrutineeDepth = 1,
-  _matchDepth = 0,
-  _auxDepth = 1,
-  _polyRecursion = True,
-  _predPolyRecursion = False,
-  _abduceScrutinees = True,
-  _unfoldLocals = False,
-  _partialSolution = False,
-  _incrementalChecking = True,
-  _consistencyChecking = False,
-  _splitMeasures = True,
-  _useMemoization = False,
-  _symmetryReduction = False,
   _context = id,
   _sourcePos = noPos,
   _explorerLogLevel = 0,
-  _buildGraph = False,
-  _useSuccinct = False,
   _solutionCnt = 1,
   _pathSearch = PetriNet,
   _useHO = False,
@@ -254,25 +169,11 @@ printDoc Html doc = putStr (showDocHtml (renderPretty 0.4 100 doc))
 
 -- | Parameters of the synthesis
 data SynquidParams = SynquidParams {
-  goalFilter :: Maybe [String],
-  outputFormat :: OutputFormat,                -- ^ Output format
-  resolveOnly :: Bool,                         -- ^ Stop after resolution step
-  repairPolicies :: Bool,
-  verifyOnly :: Bool,
-  showSpec :: Bool,                            -- ^ Print specification for every synthesis goal
-  showStats :: Bool,                            -- ^ Print specification and solution size
-  envPath :: String -- ^ Path to the environment file
+    envPath :: String -- ^ Path to the environment file
 }
 
 defaultSynquidParams = SynquidParams {
-  goalFilter = Nothing,
-  outputFormat = Plain,
-  resolveOnly = False,
-  repairPolicies = False,
-  verifyOnly = False,
-  showSpec = True,
-  showStats = False,
-  envPath = defaultEnvPath
+    envPath = defaultEnvPath
 }
 
 precomputeGraph :: [PkgName] -> [String] -> Int -> Bool -> String -> IO ()
@@ -317,4 +218,4 @@ runOnFile synquidParams explorerParams file = do
               return $ Goal id emptyEnv sch uprog 3 $ initialPos "goal"
           _ -> error "parse a signature for a none goal declaration"
 
-    pdoc = printDoc (outputFormat synquidParams)
+    pdoc = printDoc Plain
--- src/Synquid/Explorer.hs
+++ src/Synquid/Explorer.hs
@@ -24,24 +24,9 @@ data PathStrategy =
 -- | Parameters of program exploration
 data ExplorerParams = ExplorerParams {
   _eGuessDepth :: Int,                    -- ^ Maximum depth of application trees
-  _scrutineeDepth :: Int,                 -- ^ Maximum depth of application trees inside match scrutinees
-  _matchDepth :: Int,                     -- ^ Maximum nesting level of matches
-  _auxDepth :: Int,                       -- ^ Maximum nesting level of auxiliary functions (lambdas used as arguments)
-  _polyRecursion :: Bool,                 -- ^ Enable polymorphic recursion?
-  _predPolyRecursion :: Bool,             -- ^ Enable recursion polymorphic in abstract predicates?
-  _abduceScrutinees :: Bool,              -- ^ Should we match eagerly on all unfolded variables?
-  _unfoldLocals :: Bool,                  -- ^ Unfold binders introduced by matching (to use them in match abduction)?
-  _partialSolution :: Bool,               -- ^ Should implementations that only cover part of the input space be accepted?
-  _incrementalChecking :: Bool,           -- ^ Solve subtyping constraints during the bottom-up phase
-  _consistencyChecking :: Bool,           -- ^ Check consistency of function's type with the goal before exploring arguments?
-  _splitMeasures :: Bool,                 -- ^ Split subtyping constraints between datatypes into constraints over each measure
   _context :: RProgram -> RProgram,       -- ^ Context in which subterm is currently being generated (used only for logging and symmetry reduction)
-  _useMemoization :: Bool,                -- ^ Should enumerated terms be memoized?
-  _symmetryReduction :: Bool,             -- ^ Should partial applications be memoized to check for redundancy?
   _sourcePos :: SourcePos,                -- ^ Source position of the current goal
   _explorerLogLevel :: Int,               -- ^ How verbose logging is
-  _useSuccinct :: Bool,
-  _buildGraph :: Bool,
   _solutionCnt :: Int,
   _pathSearch :: PathStrategy,
   _useHO :: Bool,
