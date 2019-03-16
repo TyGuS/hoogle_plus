2fbf6ba2d9f01802c80999555d298cda1ccc6a24
Michael James <hello@michaelbjames.com>
2019-03-16 15:57:23 -0700

    include generate types

---
+++ src/Types/Generate.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module Types.Generate where
+
+import Types.Common
+
+import Language.Haskell.Exts (Decl)
+import Data.Map (Map)
+import qualified Language.Haskell.Exts.Syntax as HSE
+import Data.Data
+import Data.Typeable
+
+type Version = String
+type PkgName = String
+type MdlName = String
+
+data GenerationOpts = GenerationOpts {
+    instantiationDepth :: Int,
+    enableHOF :: Bool,
+    pkgFetchOpts :: PackageFetchOpts,
+    modules :: [String],
+    envPath :: FilePath
+    }
+    deriving (Show, Typeable, Eq)
+
+data PackageFetchOpts
+    = Hackage {
+        packages :: [String]
+        }
+    | Local {
+        files :: [String]
+        }
+    deriving (Show, Typeable, Eq)
+
+data Entry
+    = EPackage String
+    | EModule String
+    | EDecl HDeclaration
+    deriving (Data, Typeable, Show, Eq, Ord)
+
+type DependsOn = Map PkgName [Id]
+type HType = HSE.Type ()
+type HName = HSE.Name ()
+type HExp = HSE.Exp ()
+type HDeclaration = HSE.Decl ()
+
+
+defaultHackageOpts = Hackage {packages = []}
+
+defaultLocalOpts = Local {
+    files = ["working/newbase.txt"]
+    }
+
+defaultGenerationOpts = GenerationOpts {
+    instantiationDepth = 0,
+    enableHOF = True,
+    pkgFetchOpts = defaultLocalOpts,
+    modules = [],
+    envPath = defaultEnvPath
+    }
+
+defaultEnvPath = "data/env.db"
+downloadDir = "/tmp/"
