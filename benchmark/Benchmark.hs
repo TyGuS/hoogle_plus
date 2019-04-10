6ad2a79e2541868290c732a6301725b99e3ab16f
Michael James <hello@michaelbjames.com>
2019-04-10 16:49:44 -0700

    adding command line args; re-ordering queries

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -9,21 +9,34 @@ import HooglePlus.Synthesize
 import Database.Environment
 import Runner
 import BConfig
-import BTypes
+import BTypes hiding (name)
 import BOutput
 
 import Data.Aeson
 import Data.Maybe
 import Text.Pretty.Simple
+import System.Console.CmdArgs.Implicit
 import System.Timeout
 import System.Exit
 import qualified Data.Map as Map
 
+
+defaultArgs = Args {
+  argsQueryFile=defaultQueryFile &= name "queries" &= typFile,
+  argsTimeout=defaultTimeout &= name "timeout" &= help "Each experiment will have N seconds to complete" ,
+  argsOutputFile=Nothing &= name "output" &= typFile
+  }
+  where
+    defaultQueryFile = "benchmark/suites/allQueries.json"
+    defaultTimeout = 5 * 60 :: Int
+
+
 main :: IO ()
 main = do
+    args <- cmdArgs defaultArgs
     tier1env <- generateEnv genOptsTier1
     tier2env <- generateEnv genOptsTier2
-    queries <- readQueryFile queryFile
+    queries <- readQueryFile (argsQueryFile args)
     let envs = [(tier1env, "Total")]
     let params = [
           (searchParams, expQueryRefinement),
@@ -33,12 +46,15 @@ main = do
     let baseExps = mkExperiments envs queries params
     let extraExps = mkExperiments [(tier2env, "Partial")] queries [(searchParamsHOF, expQueryRefinementHOF)]
     let exps = baseExps ++ extraExps
-    resultSummaries <- runExperiments exps
+    let setup = ExpSetup {expTimeout = argsTimeout args}
+    resultSummaries <- runExperiments setup exps
     let aggregatedResults = toGroup resultSummaries
     let table = toTable aggregatedResults
-    putStrLn table
-    -- pPrint aggregatedResults
+    outputResults (argsOutputFile args) table
 
+outputResults :: Maybe FilePath -> String -> IO ()
+outputResults Nothing res = putStrLn res
+outputResults (Just fp) res = putStrLn res >> writeFile fp res
 
 mkExperiments :: [(Environment, String)] -> [Query] -> [(SearchParams, String)] -> [Experiment]
 mkExperiments envs qs params = [
@@ -51,5 +67,5 @@ readQueryFile :: FilePath -> IO [Query]
 readQueryFile fp = do
   mbQs <- decodeFileStrict' fp
   case mbQs of
-    Nothing -> undefined
+    Nothing -> error "Unable to read query file. Is the JSON poorly formatted?"
     Just qs -> return qs
