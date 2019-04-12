f65dff9bcbb17eb0c7c18897054ff3bab19625ce
Michael James <hello@michaelbjames.com>
2019-04-12 11:23:03 -0700

    icfp evaluation tier1 and generation script

---
+++ scripts/icfp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp libraries/tier1/{ghc-prim,base,bytestring,deepseq}.txt $TMPDIR
+
+stack exec -- synquid generate -h \
+      -p base \
+      -m "Data.Int" \
+    -m "Data.Maybe" \
+    -m "Data.Either" \
+    -m "GHC.List" \
+    -m "GHC.Char" \
+    -m "Text.Show" \
+    -p bytestring \
+    -m "Data.ByteString.Lazy" \
+    -m "Data.ByteString.Builder"
