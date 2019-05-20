67d3560ae3f9bf163df2ea1238b58af387ba29c8
Michael James <hello@michaelbjames.com>
2019-05-20 00:01:33 -0700

    add better skeleton printing; and a base test

--- src/Types/Abstract.hs
+++ src/Types/Abstract.hs
@@ -4,8 +4,13 @@ module Types.Abstract where
 import Types.Common
 
 import Data.Set (Set)
+import qualified Data.Set as Set
 import GHC.Generics
 import Data.Serialize
+import Data.Tree
+import Data.Tree.Pretty
+import Data.List.Extra (intercalate)
+import Text.Printf
 
 data AbstractSkeleton =
       ADatatypeT Id [AbstractSkeleton] -- explicit datatypes
@@ -28,3 +33,16 @@ data SplitInfo = SplitInfo {
     splitedPlaces :: [(AbstractSkeleton, [AbstractSkeleton])],
     splitedGroup :: [(Id, [Id])]
 } deriving (Eq, Ord, Show)
+
+prettySkeleton (ADatatypeT id sks) | length sks > 1 = unwords $ id:(map (\x -> printf "(%s)" $ prettySkeleton x) sks)
+prettySkeleton (ADatatypeT id sks) = unwords $ id:(map prettySkeleton sks)
+prettySkeleton (AExclusion ids) | null ids = "*"
+prettySkeleton (AExclusion ids) = "¬{" ++ (intercalate ", " (Set.toList ids)) ++ "}"
+prettySkeleton (ATypeVarT id) = id
+prettySkeleton (AFunctionT args ret) = prettySkeleton args ++ " → " ++ prettySkeleton ret
+
+prettyPrintAbstractionTree x = drawVerticalTree $ (prettySkeleton <$> (abstractTreetoTree x))
+  where
+    abstractTreetoTree :: AbstractionTree -> Tree AbstractSkeleton
+    abstractTreetoTree (ALeaf sk) = Node sk []
+    abstractTreetoTree (ANode sk left right) = Node sk [abstractTreetoTree left, abstractTreetoTree right]
---
+++ test/Types/PetriNet/EquivalenceSpec.hs
@@ -0,0 +1,22 @@
+module Types.PetriNet.EquivalenceSpec (spec) where
+
+import Types.PetriNet
+import PetriNet.PNBuilder
+
+import Test.Hspec
+import Text.Pretty.Simple
+
+spec :: Spec
+spec = do
+  describe "Petri Net Transition Equivalence" $ do
+    it "Should find duplicate transitions of the same type" $ do
+      let pn = buildPetriNet testFuncs []
+      pPrint pn
+      let dupes = transitionDuplicates pn
+      dupes `shouldBe` (2, 3)
+
+testFuncs = [
+  FunctionCode "head" [] ["List A"] ["A"],
+  FunctionCode "tail" [] ["List A"] ["A"],
+  FunctionCode "append" [] ["List A", "List A"] ["List A"]
+  ]
