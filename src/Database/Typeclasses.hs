277a194c69d5a9fd87d3c8a1b5a46d4d6a547cb0
David <djusto@ucsd.edu>
2019-05-25 12:50:53 -0700

    fixed many significant compiler errors

--- src/Database/Typeclasses.hs
+++ src/Database/Typeclasses.hs
@@ -4,6 +4,7 @@ module Database.Typeclasses where
     import Types.Program
     import Types.Type
     import qualified Data.Map as Map
+    import Synquid.Logic
 
     -- new data decalrations
     -- TODO: move to Types after done w/ design
@@ -11,35 +12,30 @@ module Database.Typeclasses where
     data Typeclass = Typeclass {
         dictName :: String,
         params :: [String],
-        interface :: [(String, (TypeSkeleton a))] -- TODO: can't compile due to `out of scope a`, but why?
+        interface :: [(TypeSkeleton Formula)]
     }
 
-    data DataSig = DataSig -- TODO: I'm unsure about this one
-    data FunctionSig = FunctionSig -- TODO: Isn't this FunctionT (?)
+    data DataSig = DataSig -- TODO: Isn't this DatatypeT (?)
+    data FunctionSig = FunctionSig -- TODO: Isn't this FunctionT (?) or maybe BareDeclaration (FuncDecl)
 
     -- new function declarations
     -- TODO: determine where these should be located
     -- The `Pos` stuff is really useless isn't it?
+    -- TODO: I'm not using the environment at all
     mkTypeclassDict :: Environment -> Typeclass -> ConstructorSig
     mkTypeclassDict env tyclass = ConstructorSig tyclassName tySig
-        where tySig = foldl toSignature returnTy $ interfaceAsArgs
+        where tySig = foldr toSignature returnTy $ interface tyclass
               tyclassName = dictName tyclass
 
-              -- TODO: does the function name matter? 
-              toSignature acc arg = Fun "_" $ arg acc
-
-              -- TODO: it is not necessarily true that the interface contains only functions?
-              -- I believe it may contain superclass dictionaries. But maybe those behave as
-              -- functions too?
-              interfaceAsArgs = map processInterface $ interface tyclass
-              processInterface (fname, fsig) = (FunctionT fname fsig)
+              -- TODO: does the function name matter? If so, I can create freshvars
+              toSignature acc arg = FunctionT "_" arg acc
 
               -- to get typeclass params
-              annotateParams x = ScalarT (TypeVarT Map.empty x) ftrue
-              params = map annotateParams $ params tyclass
+              annotateParams x = ScalarT (TypeVarT Map.empty "") ftrue
+              tyclassParams = map annotateParams $ params tyclass
 
               -- final signature
-              returnTy = (ScalarT (DatatypeT tyclassName params []) ftrue)
+              returnTy =  (ScalarT (DatatypeT tyclassName tyclassParams []) ftrue)
 
 
 
