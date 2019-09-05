87b2f883b0db440474e743d51ee0550b532e2da3
Ziteng Wang <mymisting@gmail.com>
2019-09-05 10:23:04 -0700

    Fix interpreter running on nondeterministic inputs (#38)

--- test/HooglePlus/FilterTestSpec.hs
+++ test/HooglePlus/FilterTestSpec.hs
@@ -36,7 +36,7 @@ testNotCrashCases =
   , ("Fail on invalid function 2", ["Data.List"], "a -> a", "\\x -> head []", False)
   , ("Fail on invalid function 3", ["Data.List"], "a -> (a, a)", "\\x -> (head [x], last [])", False)
   , ("Fail on invalid function 4", ["Data.List"], "a -> (a, a)", "\\x -> (head [x], last [])", False)
-  -- , ("Non-deterministic function", [], "Int", "last $ repeat 5", True)
+  , ("Non-deterministic function", [], "Int", "last $ repeat 5", True)
   , ("Pass w/ type class 1", [], "(Show a, Show b) => Either a b -> String", "\\x -> show x", True)]
 
 runDuplicateTest :: FilterState -> [String] -> String -> String -> IO (Bool, FilterState)
