bc990997256a24262b1a48c723edd48aa1f5c949
Ziteng Wang <mymisting@gmail.com>
2019-10-28 21:45:37 +0000

    Fix formatting on polymorphic types

--- src/Types/Filtering.hs
+++ src/Types/Filtering.hs
@@ -34,7 +34,7 @@ instance Show ArgumentType where
   show (Concrete    name) = name
   show (Polymorphic name) = name
   show (ArgTypeList sub)  = printf "[%s]" (show sub)
-  show (ArgTypeApp  l r)  = printf "(%s) %s"  (show l) (show r)
+  show (ArgTypeApp  l r)  = printf "((%s) (%s))"  (show l) (show r)
   show (ArgTypeTuple types) =
     (printf "(%s)" . intercalate ", " . map show) types
   show (ArgTypeFunc src dst) = printf "((%s) -> (%s))" (show src) (show dst)
