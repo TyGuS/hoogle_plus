3cceae659243eb16ed13084a0dd0819285d51552
Michael James <hello@michaelbjames.com>
2020-01-30 13:04:04 -0800

    includes copy to clipboard (#55)

--- webapp/app/Main.hs
+++ webapp/app/Main.hs
@@ -1,10 +1,12 @@
 import Application -- for YesodDispatch instance
 import Foundation
 import Yesod.Core
+import Yesod.Static
 import qualified Data.Map as Map
 import Data.IORef
 
 main :: IO ()
 main = do
     threadMap <- newIORef Map.empty
-    warp 3000 $ App threadMap
+    static@(Static settings) <- static "webapp/assets"
+    warp 3000 $ App threadMap static
