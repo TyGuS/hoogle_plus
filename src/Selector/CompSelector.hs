39e55bfa8f51ea7b064d24328ac6a6900781d3ea
Zheng Guo <zhg069@ucsd.edu>
2020-02-05 23:02:22 -0800

    update default packages

--- src/Selector/CompSelector.hs
+++ src/Selector/CompSelector.hs
@@ -1,24 +1,26 @@
 module Selector.CompSelector where
 
-import Types.Type
+import Database.Util
+import Synquid.Type
 import Types.Common
 import Types.Environment
-import Synquid.Type
-import Database.Util
+import Types.Type
 
+import Control.Lens
 import Data.List
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
-import Control.Lens
 import Debug.Trace
 
+defaultPkgs = ["Data.Function", "Data.Bool", "GHC.List"]
+
 selectByType :: RType -> Environment -> Environment
-selectByType query env = let
-    dts = allDatatypes query
-    defs = Set.map ((env ^. datatypes) Map.!) dts
-    mdls = "Data.Function" : "Data.Bool" : Map.keys (env ^. arguments) ++ Set.toList (Set.map _srcModule defs)
-    updatedSymbol = symbols %~ (Map.filterWithKey (\k _ -> inModules mdls k)) $ env
-    updatedEnv = typeClasses %~ (Map.filterWithKey (\k _ -> k `Set.member` dts)) $ updatedSymbol
-    in traceShow mdls updatedEnv
-    where
-        inModules mdls m = or (map (`isInfixOf` m) mdls)
+selectByType query env =
+    let dts = allDatatypes query
+        defs = Set.map ((env ^. datatypes) Map.!) dts
+        mdls = Map.keys (env ^. arguments) ++ Set.toList (Set.map _srcModule defs) ++ defaultPkgs
+        updatedSymbol = symbols %~ (Map.filterWithKey (\k _ -> inModules mdls k)) $ env
+        updatedEnv = typeClasses %~ (Map.filterWithKey (\k _ -> k `Set.member` dts)) $ updatedSymbol
+     in traceShow mdls updatedEnv
+  where
+    inModules mdls m = or (map (`isInfixOf` m) mdls)
