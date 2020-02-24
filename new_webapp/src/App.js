d454ad3a1ce3c684ebafc3b01c0d39e62345aad3
Shivani Doshi <s1doshi@ucsd.edu>
2020-02-23 17:14:42 -0800

    shivani's work

--- new_webapp/src/App.js
+++ new_webapp/src/App.js
@@ -4,14 +4,32 @@ import './App.css';
 import CandidateList from './components/CandidateList';
 import SearchBar from './components/SearchBar';
 import {TopBar} from './components/TopBar';
+import SampleQueries from './components/SampleQueries';
+import ComponentList from './components/ComponentList';
 
 
 function App() {
   return (
     <div className="App">
-      <TopBar/>
-      <SearchBar/>
-      <CandidateList/>
+      <TopBar></TopBar>
+      <SearchBar></SearchBar>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <CandidateList></CandidateList>
+      <SampleQueries></SampleQueries>
+      <ComponentList></ComponentList>
     </div>
   );
 }
