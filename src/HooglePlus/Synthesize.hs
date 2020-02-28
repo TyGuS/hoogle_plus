f1cff68ec0121252d5676825edbd97685c2d9f0a
Darya Verzhbinsky <darya99@gmail.com>
2020-02-28 11:56:53 -0800

    changed Synthesize.hs - toMonotype destType

--- src/HooglePlus/Synthesize.hs
+++ src/HooglePlus/Synthesize.hs
@@ -103,7 +103,7 @@ synthesize searchParams goal messageChan = do
     putStrLn $ "Hello world"
     let args = Monotype destinationType : Map.elems (env ^. arguments)
     print $ args
-    print $ env''' ^. symbols
+    print $ toMonotype destinationType
   -- start with all the datatypes defined in the components, first level abstraction
 
     --------------------------
