87dc70925554e7eb6f51012c33604258c1ada099
Darya Verzhbinsky <darya99@gmail.com>
2020-02-28 12:38:18 -0800

    changed Synthesize.hs - getting stuff from st'

--- src/HooglePlus/Synthesize.hs
+++ src/HooglePlus/Synthesize.hs
@@ -118,7 +118,7 @@ synthesize searchParams goal messageChan = do
     -- looking for SType
     let trial = shape destinationType
     --let blah = solveTypeConstraint env trial trial
-    putStrLn $ "trial: " ++ show (typeOf trial)
+    putStrLn $ "trial: " ++ show trial
     st' <- evalStateT (solveTypeConstraint env trial trial) initSolverState
     --putStrLn $ "st': " ++ show st'
 
