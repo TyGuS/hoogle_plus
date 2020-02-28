012fb70ee0f05b8c67ff27f576d08dcc48dfbd32
Darya Verzhbinsky <darya99@gmail.com>
2020-02-28 12:36:14 -0800

    changed Synthesize.hs - getting stuff from st'

--- src/HooglePlus/Synthesize.hs
+++ src/HooglePlus/Synthesize.hs
@@ -120,8 +120,8 @@ synthesize searchParams goal messageChan = do
     --let blah = solveTypeConstraint env trial trial
     st' <- evalStateT (solveTypeConstraint env trial trial) initSolverState
     --putStrLn $ "st': " ++ show st'
-    let substitution =  st' ^. typeAssignment
-    let checkResult = st' ^. isChecked
+    let substitution =  _typeAssignment st' -- ^. typeAssignment
+    let checkResult = _isChecked st' -- ^. isChecked
     putStrLn $ "sub: " ++ show substitution
     putStrLn $ "checked: " ++ show checkResult
     --let cons’ = stypeSubstitution substitution (shape $ toMonotype cons)
