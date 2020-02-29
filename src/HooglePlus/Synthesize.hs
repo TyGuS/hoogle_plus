b9707fec716dddacbb70c2be40374451ed029081
Darya Verzhbinsky <darya99@gmail.com>
2020-02-28 19:53:32 -0800

    changed Synthesize.hs - getting stuff from st'

--- src/HooglePlus/Synthesize.hs
+++ src/HooglePlus/Synthesize.hs
@@ -213,7 +213,7 @@ getUnifiedFunctions :: Environment -> [(Id, RSchema)] -> RType -> [(Id, RSchema)
 getUnifiedFunctions _ [] _ = []
 getUnifiedFunctions env ( v@(id, schema) : xs) goalType = do
     let initSolverState = emptySolverState
-    let t1 = shape lastType (toMonotype schema)
+    let t1 = shape (lastType (toMonotype schema))
     let t2 = shape goalType
             -- putStrLn $ "t1: " ++ show t1
             -- putStrLn $ "t2: " ++ show t2
