b14f86597ba62c044ea69b9cb307b74091a8e89b
Darya Verzhbinsky <darya99@gmail.com>
2020-02-28 21:04:05 -0800

    changed Synthesize.hs - getting stuff from st'

--- src/HooglePlus/Synthesize.hs
+++ src/HooglePlus/Synthesize.hs
@@ -222,7 +222,8 @@ synthesize searchParams goal messageChan = do
             -- getUnifiedFunctions env xs goalType
             let substitution =  st' ^. typeAssignment
             let checkResult = st' ^. isChecked
-            putStrLn $ show t1
+            --putStrLn $ show t1
+            print $ acc
 
             if (checkResult) then getUnifiedFunctions envv xs goalType (v : acc)
                             else getUnifiedFunctions envv xs goalType acc
