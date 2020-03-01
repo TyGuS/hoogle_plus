4c63eb5b48d71ac7b2d97b1745f717762d680ec1
Darya Verzhbinsky <darya99@gmail.com>
2020-03-01 15:30:30 -0800

    changed Synthesize.hs - trying to get rid of compile errors

--- src/HooglePlus/Synthesize.hs
+++ src/HooglePlus/Synthesize.hs
@@ -296,7 +296,8 @@ dfs env messageChan depth (id, schema) = do
 
   -- list2 :: [ [ IO [String] ] ]
   let list2  = map (map (dfs env messageChan (depth - 1))) argUnifiedFuncs'
-  fmap putStrLn list2
+  list3 <- sequence list2
+  -- fmap putStrLn list2
   -- print $ typeOf  list2
 
   -- print $ typeOf list2
