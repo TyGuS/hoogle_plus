5caadce859d8bcadf50eef8dc0428e0bbd369639
Shivani Doshi <s1doshi@ucsd.edu>
2020-03-02 10:33:51 -0800

    deleted unnecessary files

--- new_webapp/src/utilities/args 2.js
+++
@@ -1,7 +0,0 @@
-export const getArgNames = (numArgs) => {
-    let argNames = [];
-    for (let index = 0; index < numArgs; index++) {
-      argNames = argNames.concat("arg" + index);
-    }
-    return argNames;
-}
