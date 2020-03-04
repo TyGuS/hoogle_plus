23f728998774e705083721076841b8617cf8143e
Darya Verzhbinsky <darya99@gmail.com>
2020-03-04 12:33:35 -0800

    trying to fix returnType issue

--- src/HooglePlus/Synthesize.hs
+++ src/HooglePlus/Synthesize.hs
@@ -258,10 +258,10 @@ getUnifiedFunctions envv messageChan xs goalType = do
   lift $ putStrLn $ "goalType: " ++ show goalType
   -- lift $ putStrLn $ "args: " ++ show ()
   
-  if (not $ ground goalType) -- sometimes it returns (a -> b) - need to make sure we do just one of those
+  if (not $ isGround goalType) -- sometimes it returns (a -> b) - need to make sure we do just one of those
     then do
       let args = allArgTypes goalType
-      return $ mapM (getUnifiedFunctions envv messageChan xs) args
+      map (getUnifiedFunctions envv messageChan xs) args
     else do
       modify $ set components []
       st <- get
