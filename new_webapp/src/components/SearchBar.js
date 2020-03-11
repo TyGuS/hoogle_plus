335140a7478cb5d1ce27077a4d4222975c4f3777
Michael James <hello@michaelbjames.com>
2020-03-11 14:26:22 -0700

    remove dead component

--- new_webapp/src/components/SearchBar.js
+++ new_webapp/src/components/SearchBar.js
@@ -23,7 +23,7 @@ const mapStateToProps = (state) => {
     }
 };
 
-const mySearchBar = (props) => {
+const connectedSearchBar = (props) => {
     const {searchType, exampleRows, numArgs} = props;
     const {setSearchType, doSearch, getTypesFromExamples} = props;
     const {search} = getDefaultFeatures();
@@ -90,89 +90,6 @@ const mySearchBar = (props) => {
     );
 }
 
-class ConnectedSearchBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ""};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        const usages = this.props.exampleRows.map(({usage}) => usageToExample(usage));
-        if (this.isMissingType()){
-            this.props.getTypesFromExamples(usages);
-            return;
-        }
-        const filteredUsages = _.filter(usages, usageList => !_.any(usageList, _.isUndefined));
-        this.props.setSearchType({query: this.state.value, examples: filteredUsages});
-        this.props.doSearch({query: this.state.value, examples: filteredUsages});
-    }
-
-    canSubmit() {
-        return (!_.isEmpty(this.props.exampleRows)) || (!this.isMissingType());
-    }
-
-    isMissingType() {
-        return this.state.value === "";
-    }
-
-    render() {
-        const {search} = getDefaultFeatures();
-        return (
-            <div>
-            <TypeSelection hidden={!search.permitTypeCandidates} />
-            <div className="container">
-                <Form onSubmit={this.onSubmit}>
-                <div className="row justify-content-center">
-                <InputGroup className="mb-3 col-8">
-                    <FormControl
-                    aria-label="Default"
-                    aria-describedby="inputGroup-sizing-default"
-                    placeholder="Search by type here"
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                    />
-                </InputGroup>
-                {/*
-                    <InputGroup.Text
-                    type="text"
-                    name="value"
-                    placeholder="search by type here"
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                    className="col-8"
-                    /> */}
-                </div>
-                <div
-                    className="row justify-content-center"
-                    hidden={!search.permitExamples}>
-                    <div className="col">
-                        <div>
-                            Example Specifications:
-                        </div>
-                        <ExampleTable/>
-                    </div>
-                </div>
-                <Button
-                    disabled={!this.canSubmit()}
-                    onClick={this.handleSubmit}
-                    type="submit">
-                    Search
-                </Button>
-                </Form>
-            </div>
-
-            </div>
-        );
-    }
-}
-
-const SearchBar = connect(mapStateToProps, mapDispatchToProps)(mySearchBar);
+const SearchBar = connect(mapStateToProps, mapDispatchToProps)(connectedSearchBar);
 
 export default SearchBar;
