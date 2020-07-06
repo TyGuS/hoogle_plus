b64adcc80c6325196bc7927e0f412fdfd1e53c9c
EpicOrange@github.com <danieldoesntcheckthis@gmail.com>
2020-07-06 19:25:04 +0000

    turned dfs into one function instead of dfsTop and dfs

--- src/TopDown/Synthesize.hs
+++ src/TopDown/Synthesize.hs
@@ -29,7 +29,7 @@ import Types.IOFormat
 import HooglePlus.Utils
 import HooglePlus.IOFormat
 import Examples.ExampleChecker
-import PetriNet.Util -- TODO can we do this? lol 
+import PetriNet.Util
 
 import Control.Applicative ((<$>))
 import Control.Concurrent.Chan
@@ -111,23 +111,39 @@ synthesize searchParams goal examples messageChan = do
     let numArgs = length (Map.elems (envWithHo ^. arguments))
 
     -- TIMING STUF
-    start <- getCPUTime
-    printf "running dfsTop on %s\n" (show $ shape destinationType)
+    -- start <- getCPUTime
+    -- printf "running dfsTop on %s\n" (show $ shape destinationType)
 
-    foo <- dfsTop envWithHo messageChan 3 (shape destinationType) numArgs
-    
-    printf "done running dfsTop on %s\n" (show $ shape destinationType)
+    solutions <- evalCompsSolver messageChan $ dfsNew envWithHo messageChan 3 (shape destinationType) :: IO [RProgram]
 
-    end <- getCPUTime
+    mapM print $ take 2 $ filter (filterParams numArgs) solutions
+
+    -- let samplePrograms' = map show samplePrograms :: [String] 
+    -- let filtered = filter (filterParams numArgs) samplePrograms'
+    -- unless (null filtered) (liftIO $ putStrLn $ head filtered) -- we print out the first result for each function
+    -- return samplePrograms'
+    -- TODO do filtering stuff here with numArgs
     
-    let diff = fromIntegral (end - start) / (10^12)
-    printf "Computation time: %0.3f sec\n" (diff :: Double)
+    -- printf "done running dfsTop on %s\n" (show $ shape destinationType)
+
+    -- end <- getCPUTime
+    
+    -- let diff = fromIntegral (end - start) / (10^12)
+    -- printf "Computation time: %0.3f sec\n" (diff :: Double)
 ------------
     writeChan messageChan (MesgClose CSNormal)
     return ()
 
+    where
+      -- determines if the result has all the appropriate arguments given the number of args
+      filterParams :: Int -> RProgram -> Bool
+      filterParams 0       _ = error "filterParams error: shouldn't have 0 args!" -- TODO maybe should be true here? 
+      filterParams 1       x = "arg0" `isInfixOf` (show x)
+      filterParams numArgs x = isInfixOf ("arg" ++ (show (numArgs - 1))) (show x) && filterParams (numArgs - 1) x
+
 
 type CompsSolver m = StateT Comps (StateT CheckerState m)
+evalCompsSolver messageChan m = m `evalStateT` emptyComps `evalStateT` (emptyChecker { _checkerChan = messageChan })
 
 instance Monad m => CheckMonad (CompsSolver m) where
     getNameCounter = lift getNameCounter
@@ -139,42 +155,162 @@ instance Monad m => CheckMonad (CompsSolver m) where
     getMessageChan = lift getMessageChan
     overStats      = lift . overStats
 
-
 --
 -- start off calling dfs with an empty memoize map
 --
-dfsTop :: Environment -> Chan Message -> Int -> SType -> Int -> IO [String] -- TODO change to RProgram
-dfsTop env messageChan depth hole numArgs = helper `evalStateT` emptyComps `evalStateT` (emptyChecker { _checkerChan = messageChan })
+
+-- convert into 1 DFS function with signature: Environment -> Int -> SType -> [RProgram]
+
+-- brainstorming here!!!
+
+{--
+    takes in single type (return type)          e.g. Int
+    finds all functions that unify with it      e.g. [f: Int, g: Int -> Int, h: Int -> Bool -> Int]
+      for each of these functions F:
+        if not ground:                          e.g. [Int -> Int, Int -> Bool -> Int]
+          get arguments of F                    e.g. [[Int], [Int, Bool]]
+          for each argument:
+            call dfs on the argument to get the programs that create that argument
+                                                e.g. Int becomes [f,g,h], Bool becomes []
+          now you have a list of [RProgram] for each argument
+          take our function F (e.g. h: Int -> Bool -> Int) and fill in the arguments using the [RProgram] we get
+          to get a new list of [RProgram]       e.g. if F is g, then return [g f]
+          we somehow take these programs for each argument and combine them into a bunch of larger programs
+    then we return [RProgram] that build up the goalType
+--}
+
+
+dfsNew :: Environment -> Chan Message -> Int -> SType -> CompsSolver IO [RProgram]
+-- dfsNew env messageChan depth goalType = evalCompsSolver messageChan $ do
+dfsNew env messageChan depth goalType = do
+  
+  -- collect all the component types (which we might use to fill the holes)
+  let components = Map.toList (env ^. symbols)
+
+  -- find all functions that unify with goal type
+  unifiedFuncs <- getUnifiedFunctions env messageChan components goalType :: CompsSolver IO [(Id, SType)]
+
+  -- for each of these functions, find solutions
+  functionSolutions <- mapM turnFunctionIntoSolutions unifiedFuncs :: CompsSolver IO [[RProgram]] -- [solutions for func1, solutions for func2]
+  let allFunctionSolutions = concat functionSolutions :: [RProgram]
+  return allFunctionSolutions
+  
+
   where
-    helper :: CompsSolver IO [String]
-    helper = do
+    -- checks if a program is ground (has no more arguments to synthesize - aka function w/o args)
+    isGround :: SType -> Bool
+    isGround (FunctionT id arg0 arg1) = False
+    isGround _ = True
 
-      -- collect all the component types (which we might use to fill the holes)
-      let components = Map.toList (env ^. symbols)
+    turnFunctionIntoSolutions :: (Id, SType) -> CompsSolver IO [RProgram]
+    turnFunctionIntoSolutions (id, schema)
+      | isGround schema = return [Program { content = PSymbol id, typeOf = refineTop env schema }]
+      | depth == 0 = return []  -- stop if depth is 0
+      | otherwise = do
 
-      -- map each hole ?? to a list of component types that unify with the hole
-      unifiedFuncs <- getUnifiedFunctions env messageChan components hole :: CompsSolver IO [(Id, SType)]
-      liftIO $ putStrLn $ show unifiedFuncs
+        -- collect all the argument types (the holes ?? we need to fill)
+        let args = allArgTypes schema :: [SType]
 
-      -- get the first valid program from each of the functions in unifiedFuncs
-      fmap concat $ mapM getFirstValidProgram unifiedFuncs :: CompsSolver IO [String]
+        -- recursively call DFS on the arguments' types to get the functions that return those types
+        let recurse = dfsNew env messageChan (depth-1) :: SType -> CompsSolver IO [RProgram]
+        solutionsPerArg <- mapM recurse args :: CompsSolver IO [[RProgram]] -- [[a,b,c], [d,e,f]]
+
+        -- solutionsPerArg <- mapM recurse args :: CompsSolver IO [[RProgram]] -- [[a,b,c], [d,e,f]]
+        
+        -- get cartesian product of all the arguments
+        let programsPerArg = sequence solutionsPerArg :: [[RProgram]] -- [[a,d], [a,e], [a,f], [b,d], [b,e], [b,f]]
+
+        -- fill in arguments of func - [func a c, func a d, func b c, func b d]
+        let formatFn :: [RProgram] -> RProgram
+            formatFn args = Program { content = PApp id args, typeOf = refineTop env schema }
+        
+        let finalResultList = map formatFn programsPerArg
+
+        return finalResultList
+
+
+
+
+-- dfsTop :: Environment -> Chan Message -> Int -> SType -> Int -> IO [String] -- TODO change to RProgram
+-- dfsTop env messageChan depth hole numArgs = evalCompsSolver messageChan helper
+--   where
+--     helper :: CompsSolver IO [String]
+--     helper = do
+
+--       -- collect all the component types (which we might use to fill the holes)
+--       let components = Map.toList (env ^. symbols)
+
+--       -- map each hole ?? to a list of component types that unify with the hole
+--       unifiedFuncs <- getUnifiedFunctions env messageChan components hole :: CompsSolver IO [(Id, SType)]
+--       -- liftIO $ putStrLn $ show unifiedFuncs
+
+--       -- get the first valid program from each of the functions in unifiedFuncs
+--       fmap concat $ mapM getFirstValidProgram unifiedFuncs :: CompsSolver IO [String]
     
-    -- get the first valid program that matches the given component
-    -- TODO right now it returns a list of all of the functions as a [String]
-    -- TODO   change it to return a single RProgram
-    getFirstValidProgram :: (Id, SType) -> CompsSolver IO [String]
-    getFirstValidProgram comp = do 
-                      samplePrograms <- dfs env messageChan depth comp :: CompsSolver IO [RProgram]
-                      let samplePrograms' = map show samplePrograms :: [String] -- TODO we should not map show; just return single RProgram
-                      let filtered = filter (filterParams numArgs) samplePrograms'
-                      unless (null filtered) (liftIO $ putStrLn $ head filtered) -- we print out the first result for each function
-                      return samplePrograms'
+--     -- get the first valid program that matches the given component
+--     -- TODO right now it returns a list of all of the functions as a [String]
+--     -- TODO   change it to return a single RProgram
+--     getFirstValidProgram :: (Id, SType) -> CompsSolver IO [String]
+--     getFirstValidProgram comp = do 
+--                       samplePrograms <- dfs env messageChan depth comp :: CompsSolver IO [RProgram]
+--                       let samplePrograms' = map show samplePrograms :: [String] -- TODO we should not map show; just return single RProgram
+--                       let filtered = filter (filterParams numArgs) samplePrograms'
+--                       unless (null filtered) (liftIO $ putStrLn $ head filtered) -- we print out the first result for each function
+--                       return samplePrograms'
     
-    -- determines if the result has all the appropriate arguments given the number of args
-    filterParams :: Int -> String -> Bool
-    filterParams 0       _ = error "filterParams error: shouldn't have 0 args!" -- TODO maybe should be true here? 
-    filterParams 1       x = "arg0" `isInfixOf` x
-    filterParams numArgs x = isInfixOf ("arg" ++ (show (numArgs - 1))) x && filterParams (numArgs - 1) x
+--     -- determines if the result has all the appropriate arguments given the number of args
+--     filterParams :: Int -> String -> Bool
+--     filterParams 0       _ = error "filterParams error: shouldn't have 0 args!" -- TODO maybe should be true here? 
+--     filterParams 1       x = "arg0" `isInfixOf` x
+--     filterParams numArgs x = isInfixOf ("arg" ++ (show (numArgs - 1))) x && filterParams (numArgs - 1) x
+
+-- -- 
+-- -- runs dfs of given depth and keeps trying to find complete programs (no filtering yet)
+-- --
+-- dfs :: Environment -> Chan Message -> Int -> (Id, SType) -> CompsSolver IO [RProgram]
+-- dfs env messageChan depth (id, schema)
+--   | isGround schema = return $ [
+--       Program { content = PSymbol id, typeOf = refineTop env schema }
+--     ]
+--   | depth == 0 = return []  -- stop if depth is 0
+--   | otherwise = do
+
+--     st <- get
+
+--     -- collect all the argument types (the holes ?? we need to fill)
+--     let args = allArgTypes schema
+
+--     -- collect all the component types (which we might use to fill the holes)
+--     let components = Map.toList (env ^. symbols)
+
+--     -- map each hole ?? to a list of component types that unify with the hole
+--     argUnifiedFuncs <- mapM (getUnifiedFunctions env messageChan components) args :: CompsSolver IO [[(Id, SType)]]
+
+--     -- recurse, solving each unified component as a goal, solution is a list of programs
+--     -- the first element of solutionsPerArg is the list of solutions for the first argument
+--     -- e.g. 
+--     let recurse = dfs env messageChan (depth - 1)
+--     solutionsPerArg <- mapM (fmap concat . mapM recurse) argUnifiedFuncs :: CompsSolver IO [[RProgram]] -- [[a,b,c], [d,e,f]]
+    
+--     -- each arg hole is a list of programs
+--     -- take cartesian product of args and prepend our func name
+--     -- to get the list of resulting programs solving our original goal
+--     -- the first element of programsPerArg is a list of programs that fit as first argument
+--     let programsPerArg = sequence solutionsPerArg :: [[RProgram]] -- [[a,d], [a,e], [a,f], [b,d], [b,e], [b,f], ...]
+--     let formatFn :: [RProgram] -> RProgram
+--         formatFn args = Program { content = PApp id args, typeOf = refineTop env schema }
+--     let finalResultList = map formatFn programsPerArg
+--     return finalResultList
+  
+--   where
+--     -- checks if a program is ground (has no more arguments to synthesize - aka function w/o args)
+--     isGround :: SType -> Bool
+--     isGround (FunctionT id arg0 arg1) = False
+--     isGround _ = True
+
+
+
+
 
 --
 -- gets list of components/functions that unify with a given type
@@ -232,47 +368,3 @@ getUnifiedFunctions envv messageChan xs goalType = do
           else return ()
 
         helper envv messageChan ys goalType
-
--- 
--- runs dfs of given depth and keeps trying to find complete programs (no filtering yet)
---
-dfs :: Environment -> Chan Message -> Int -> (Id, SType) -> CompsSolver IO [RProgram]
-dfs env messageChan depth (id, schema)
-  | isGround schema = return $ [
-      Program { content = PSymbol id, typeOf = refineTop env schema }
-    ]
-  | depth == 0 = return []  -- stop if depth is 0
-  | otherwise = do
-
-    st <- get
-
-    -- collect all the argument types (the holes ?? we need to fill)
-    let args = allArgTypes schema
-
-    -- collect all the component types (which we might use to fill the holes)
-    let components = Map.toList (env ^. symbols)
-
-    -- map each hole ?? to a list of component types that unify with the hole
-    argUnifiedFuncs <- mapM (getUnifiedFunctions env messageChan components) args :: CompsSolver IO [[(Id, SType)]]
-
-    -- recurse, solving each unified component as a goal, solution is a list of programs
-    -- the first element of solutionsPerArg is the list of solutions for the first argument
-    -- e.g. 
-    let recurse = dfs env messageChan (depth - 1)
-    solutionsPerArg <- mapM (fmap concat . mapM recurse) argUnifiedFuncs :: CompsSolver IO [[RProgram]] -- [[a,b,c], [d,e,f]]
-    
-    -- each arg hole is a list of programs
-    -- take cartesian product of args and prepend our func name
-    -- to get the list of resulting programs solving our original goal
-    -- the first element of programsPerArg is a list of programs that fit as first argument
-    let programsPerArg = sequence solutionsPerArg :: [[RProgram]] -- [[a,d], [a,e], [a,f], [b,d], [b,e], [b,f], ...]
-    let formatFn :: [RProgram] -> RProgram
-        formatFn args = Program { content = PApp id args, typeOf = refineTop env schema }
-    let finalResultList = map formatFn programsPerArg
-    return finalResultList
-  
-  where
-    -- checks if a program is ground (has no more arguments to synthesize - aka function w/o args)
-    isGround :: SType -> Bool
-    isGround (FunctionT id arg0 arg1) = False
-    isGround _ = True
