fe03942152cbb4cb3f07ad262f4e262c291c96b7
zheng <zhg069@ucsd.edu>
2020-07-20 00:20:53 -0700

    put backend ip address

--- new_webapp/src/utilities/fetches.js
+++ new_webapp/src/utilities/fetches.js
@@ -2,7 +2,7 @@
 // REACT_APP_DEVELOPMENT_HOST="<hostname>:<port>" yarn start
 // to point the backend wherever you need it to
 const devBase = "http://" + (process.env.REACT_APP_DEVELOPMENT_HOST || "localhost:5000") + "/"
-export const baseRoute = process.env.REACT_APP_DEVELOPMENT ? devBase : "/"
+export const baseRoute = process.env.REACT_APP_DEVELOPMENT ? devBase : "http://18.220.20.36:5000/"
 
 export const handleFetch = (data) => {
     return data.json()
