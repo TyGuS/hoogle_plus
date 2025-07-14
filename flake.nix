3ad44ce91b6850bab48c525d41adab696f04a146
Ziteng Wang <z@wzt.me>
2025-07-14 18:32:32 +0000

    working nix derivation

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "hoogle_plus";
+  description = "Nix derivation for HooglePlus";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
@@ -23,20 +23,27 @@
     packages = forAllSystems (system: let
       pkgs-2003 = import nixpkgs-2003 {
         inherit system;
+
+        # packdeps is marked as broken in 20.03 but seems to work fine
         config.allowBroken = true;
       };
+
+      # compile ghc844's base module with the `-fno-omit-yields` flag
+      # https://downloads.haskell.org/~ghc/8.4.4/docs/html/users_guide/bugs.html#bugs-in-ghc
       ghc844-no-omit-yields = pkgs-2003.haskell.compiler.ghc844.overrideAttrs (old: {
         patches = old.patches ++ [./base-no-omit-yields.patch];
       });
 
       hPkgs844 = pkgs-2003.haskell.packages.ghc844.extend (self: super: {
+        ghc = ghc844-no-omit-yields;
+
+        # package version consistent with lts-12.26
         haskell-src-exts = self.callHackage "haskell-src-exts" "1.20.3" {};
         hoogle = self.callHackage "hoogle" "5.0.17.3" {};
       });
 
       pkgs = import nixpkgs {
         inherit system;
-        config.allowUnfree = true;
         overlays = [
           (final: prev: {
             inherit (pkgs-2003) z3;
@@ -48,18 +55,18 @@
       inherit ghc844-no-omit-yields;
 
       hoogle = hPkgs844.hoogle;
-      hplus = hPkgs844.mkDerivation rec {
+      hplus = hPkgs844.mkDerivation {
         pname = "HooglePlus";
         version = "0-unstable-20250713";
         src = ./.;
-        sha256 = "4b1a5c8d3f0e2f6c7b8c9e2d3f4e5f6a7b8c9e2d3f4e5f6a7b8c9e2d3f4e5f6";
         doCheck = false;
         doHaddock = false;
         license = "MIT";
 
         isLibrary = true;
         isExecutable = true;
-        librarySystemDepends = with pkgs; [
+        librarySystemDepends = with pkgs;
+        with hPkgs844; [
           ncurses
           gmp
           glibcLocales
@@ -68,7 +75,7 @@
           pkgs-2003.z3
         ];
 
-        buildDepends = with hPkgs844; [
+        libraryHaskellDepends = with hPkgs844; [
           haskell-src-exts
           ChasingBottoms
           MissingH
@@ -96,30 +103,25 @@
           silently
           sort
           z3
+
+          pkgs-2003.makeWrapper
         ];
+
+        # hplus uses hint to dynamically evaluate Haskell code using ChasingBottoms
+        postInstall = let
+          ghc-with-cb = hPkgs844.ghcWithPackages (pkgs: with pkgs; [ChasingBottoms]);
+        in ''
+          wrapProgram $out/bin/hplus \
+            --set GHC_PACKAGE_PATH "${ghc-with-cb}/lib/ghc-${ghc-with-cb.version}/package.conf.d:"
+        '';
       };
     });
 
     devShells = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      pkgs-2003 = nixpkgs-2003.legacyPackages.${system};
     in {
       default = pkgs.mkShell {
-        shellHook = ''
-          export LANG=en_US.UTF-8
-          export LC_ALL=en_US.UTF-8
-          export LC_CTYPE=en_US.UTF-8
-          export LD_LIBRARY_PATH="${pkgs.zlib}/lib:$LD_LIBRARY_PATH"
-        '';
-
         packages = with pkgs; [
-          stack
-          ncurses
-          gmp
-          glibcLocales
-          libffi
-          zlib
-          pkgs-2003.z3
           self.outputs.packages.${system}.hoogle
           self.outputs.packages.${system}.hplus
 
